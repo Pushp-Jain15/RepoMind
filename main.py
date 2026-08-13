a25e6689b7f6c76ceedf14e2eea2a9c9c1ef3e44
Pushp <jainpushp123@gmail.com>
2026-08-14 00:01:43 +0530

    Day 2: GitHub API repository analyzer

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-print("Welcome to RepoMind!")
-print("AI-powered Software Engineering Intelligence")
+import requests
+
+
+def get_repository(owner, repo):
+
+    url = f"https://api.github.com/repos/{owner}/{repo}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error:", response.status_code)
+        return None
+
+
+data = get_repository("octocat", "Hello-World")
+
+if data:
+    print("Repository:", data["name"])
+    print("Owner:", data["owner"]["login"])
+    print("Stars:", data["stargazers_count"])
+    print("Forks:", data["forks_count"])
+    print("Issues:", data["open_issues_count"])
+    print("Language:", data["language"])
