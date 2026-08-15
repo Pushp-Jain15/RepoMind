c63a5e0ec71ece3910e3595f54a2b163ef724ce2
Pushp <jainpushp123@gmail.com>
2026-08-16 00:39:08 +0530

    Day 3: GitHub repository data collection

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import requests
-
-
 def get_repository(owner, repo):
-
     url = f"https://api.github.com/repos/{owner}/{repo}"
-
     response = requests.get(url)
-
     if response.status_code == 200:
         return response.json()
     else:
         print("Error:", response.status_code)
         return None
-
-
 data = get_repository("octocat", "Hello-World")
-
 if data:
     print("Repository:", data["name"])
     print("Owner:", data["owner"]["login"])
